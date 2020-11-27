bbf876f9b6ea90ec38649d6be1276c2f6bc63b4c
John McLear <john@mclear.co.uk>
2020-11-27 19:55:48 +0000

    autofixes from Etherpad checkPlugins.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var path = require('path');
-var eejs = require("ep_etherpad-lite/node/eejs");
-var settings = require('ep_etherpad-lite/node/utils/Settings');
+const path = require('path');
+const eejs = require('ep_etherpad-lite/node/eejs');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
 
-exports.clientVars = function(hook, context, callback){
-  var pad_title;
+exports.clientVars = function (hook, context, callback) {
+  let pad_title;
   try {
-    if (settings.ep_pad_title){
-        pad_title = settings.ep_pad_title;
+    if (settings.ep_pad_title) {
+      pad_title = settings.ep_pad_title;
     }
-  } catch (e){
-    console.warn("ep_pad_title: a default title can be set in settings.json");
-    pad_title = "";
+  } catch (e) {
+    console.warn('ep_pad_title: a default title can be set in settings.json');
+    pad_title = '';
   }
-  return callback({ "pad_title": pad_title });
+  return callback({pad_title});
 };
 
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
-  args.content = '<script src="../static/plugins/ep_pad_title/static/js/pad_title.js"></script>' + args.content;
+  args.content = `<script src="../static/plugins/ep_pad_title/static/js/pad_title.js"></script>${args.content}`;
   return cb();
 };
--- static/js/pad_title.js
+++ static/js/pad_title.js
@@ -1,8 +1,8 @@
 var pad_title = {
-  init: function() {
-    var pad_title = clientVars.pad_title;
-    if(pad_title){
+  init() {
+    const pad_title = clientVars.pad_title;
+    if (pad_title) {
       window.document.title = pad_title;
     }
-  }
-}
+  },
+};
